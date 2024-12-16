d4b605bb1ce90c92d16d6cede4806e4b9ea75abd
Gresia Cristian Gustavo <gustavogresia22@gmail.com>
2024-12-16 12:30:17 -0300

    creacion de los modelos de clase

--- src/gestoria/models.py
+++ src/gestoria/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class  Consorcio(models.Model):
+    domicilio =models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.domicilio
+    
+
+class  Liquidaciones(models.Model):
+    agregar =models.CharField(Consorcio, on_delete=models.SET_NULL, null=True )
+    cerrar =models.CharField(Consorcio, on_delete=models.SET_NULL, null=True )
+    abrir =models.CharField(Consorcio)    
+
+class  Unidades(models.Model):
+    nombre =models.CharField(Liquidaciones, on_delete=models.SET_NULL, null=True )
+    apellido =models.CharField(Liquidaciones, on_delete=models.SET_NULL, null=True )
+    piso_depto =models.PositiveIntegerField()
+
+
+    
